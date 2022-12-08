d8befca10e1f9cd73da666fbc8954d0021cd8231
williyem <hinnehwilliams@gmail.com>
2022-12-08 15:39:47 +0000

    feat: changes

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -2,7 +2,7 @@
 export const baseUrl: string = "http://139.144.173.250:4000/api/v1/";
 
 export const URL = {
-  getProducts: "products",
+  getProducts: "products/",
   createProduct: "products/create-product",
   signIn: "auth/signin",
   signup: "auth/signup",
