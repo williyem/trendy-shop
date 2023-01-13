2ffa833ccdeefd380028d8d0d7b94443047a252e
williyem <hinnehwilliams@gmail.com>
2023-01-13 11:48:38 +0000

    feat: changes

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -1,5 +1,6 @@
 // export const baseUrl: string = "http://139.144.173.250:4000/api/v1/";
-export const baseUrl: string = "http://localhost:4000/api/v1/";
+// export const baseUrl: string = "http://localhost:4000/api/v1/";
+export const baseUrl: string = "http://139.144.173.250:4000/api/v1/";
 
 export const URL = {
   getProducts: "products/",
